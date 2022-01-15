9ce709b897522ed5182ce967a458311b90acee89
Tasha <livegrafics@mail.ru>
2022-01-15 17:18:08 +0300

    Добавлен цикл в тест площадь прямоугольника

--- src/test/java/TestAreaRectangle.java
+++ src/test/java/TestAreaRectangle.java
@@ -5,8 +5,10 @@ public class TestAreaRectangle {
     @Test
     public void testAreaRectangle () {
         Rectangle r = new Rectangle(0, 0);
-        Assert.assertEquals(r.area(),40);
+        Assert.assertEquals(r.area(),0);
 
-
+        for (r.a = 0; r.a < 100; r.a++)
+            for (r.b = 0; r.b < 100; r.b++)
+                Assert.assertEquals(r.area(), r.a * r.b);
     }
 }
