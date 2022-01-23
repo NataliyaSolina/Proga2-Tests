f740d3cf9e4ddc063b8576163394235139ea1733
Tasha <livegrafics@mail.ru>
2022-01-23 15:13:20 +0300

    Играюсь с типами, коммитнуть дал только принудительно из-за с3)

--- src/main/java/Proga02.java
+++ src/main/java/Proga02.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Proga02 {
     public static void main(String[] args) {
         Square sq = new Square(7);
@@ -11,5 +13,17 @@ public class Proga02 {
         Circle cl = new Circle(5);
         System.out.println("Площадь круга с радиусом " + cl.r + " = " + cl.area());
         System.out.println("Периметр круга с радиусом " + cl.r + " = " + cl.perimeter());
+
+        int a = 17;
+        int b = 5;
+        float c0 = 1;
+        int c1 = a / b;
+        float c2 = 17f / 5f;
+        float c3 = a / b;
+        float c4 = (float) a / (float) b;
+        float c5 = (float) (a / b);
+        System.out.println("a= " + a + " b= " + b + " c0= " +c0 + " c1= " + c1 + " c2= " + c2 + " c3= " + c3 + " c4= " + c4 + " c5= " + c5);
+
+
     }
 }
